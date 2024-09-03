ae446e4adf0ba750eb9b9d9f0ab33697f9d444c9
suyeon <sroovy@naver.com>
2024-09-03 16:36:34 +0900

    [style] tailwind globalstyle 설정

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Next.js App Router를 사용하는 경우
+    "./pages/**/*.{js,ts,jsx,tsx}", // Page Router를 사용하는 경우
+    "./components/**/*.{js,ts,jsx,tsx}", // 공통 컴포넌트
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
