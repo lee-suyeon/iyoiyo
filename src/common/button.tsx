ab6c382901d25f6afcad71e6e5dfd25846295672
suyeon <sroovy@naver.com>
2024-09-04 19:36:28 +0900

    [feat] Button 컴포넌트 Props 추가
    - full width를 위한 props 추가

--- src/common/button.tsx
+++ src/common/button.tsx
@@ -1,14 +1,21 @@
 interface ButtonProps {
   primary?: boolean;
+  fullWidth?: boolean;
   children: React.ReactNode;
 }
 
-export default function Button({ children, primary = true }: ButtonProps) {
+export default function Button({
+  children,
+  fullWidth,
+  primary = true,
+}: ButtonProps) {
+  const buttonWidth = fullWidth ? "w-full" : "";
+
   return (
     <button
       className={`${
         primary ? "bg-blue-950" : "bg-blue-200"
-      } text-white py-3 px-4 rounded-full min-w-80 text-sm`}
+      } text-white py-3 px-4 rounded-full ${buttonWidth}`}
     >
       {children}
     </button>
