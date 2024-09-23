4b0136539362b5b029a3da06d9704d71c123246a
suyeon <sroovy@naver.com>
2024-09-23 22:41:33 +0900

    [feat] Date Selector 컴포넌트 추가
    * react-datepicker 라이브러리

---
+++ src/common/DateSelector.tsx
@@ -0,0 +1,24 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import classes from "./date-selector.module.css";
+
+interface DateSelectorProps {
+  selected?: Date | null;
+  // onChange: (value: string) => void;
+  name: string;
+  label?: string;
+}
+
+export default function DateSelector({
+  selected = new Date(),
+  // onChange,
+  label,
+  name,
+}: DateSelectorProps) {
+  return (
+    <div className={classes.container}>
+      {label && <label className={classes.label}>{label}</label>}
+      <DatePicker selected={selected} dateFormat="yyyy.MM.dd" />
+    </div>
+  );
+}
--- src/common/index.ts
+++ src/common/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button";
 import TextInput from "./TextInput";
 import CheckBox from "./CheckBox";
 import ImagePicker from "./ImagePicker";
+import DateSelector from "./DateSelector";
 
-export { Button, TextInput, CheckBox };
+export { Button, TextInput, CheckBox, ImagePicker, DateSelector };
